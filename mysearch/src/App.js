d09e1668d936d7d083dbef11414783b26a0f6ca6
Judiann004 <judiannpraise004gmail.com>
2023-07-11 16:53:34 +0100

    first commit

--- mysearch/src/App.js
+++ mysearch/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
       return (
         <div className="App">
-         <div className="container">
+         <
+          div className="container">
           <Weather defaultCity="ibadan" />
           <footer>
             The project was coded by <a href="mailto:judiannpraise004@gmail">Judiann Praise</a> and is {""} <a href="/">open- sourced on Github</a>
