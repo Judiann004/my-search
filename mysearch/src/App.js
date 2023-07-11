b07ef3d476f4902d6b50839f59d43aeb80a247a1
Judiann004 <judiannpraise004gmail.com>
2023-07-11 18:41:44 +0100

    build commit

--- mysearch/src/App.js
+++ mysearch/src/App.js
@@ -9,7 +9,7 @@ function App() {
           div className="container">
           <Weather defaultCity="ibadan" />
           <footer>
-            The project was coded by <a href="mailto:judiannpraise004@gmail">Judiann Praise</a> and is {""} <a href="/">open- sourced on Github</a>
+            The project was coded by <a href="mailto:judiannpraise004@gmail">Judiann Praise</a> and is {""} <a href="https://github.com/Judiann004/my-search">open- sourced on Github</a>
           </footer>
           
           </div>
